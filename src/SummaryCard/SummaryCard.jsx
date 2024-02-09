df1eeb54edcd792e03e59676985ffd03858e07d2
mattcbodily <mattcbodily@gmail.com>
2024-02-08 20:16:00 -0700

    finishes SummaryCard design

--- src/SummaryCard/SummaryCard.jsx
+++ src/SummaryCard/SummaryCard.jsx
@@ -6,51 +6,67 @@ import "./SummaryCard.css"
 
 export const SummaryCard = () => {
   return (
-    <div className='summary-card-wrapper'>
-      <div className='summary-card'>
-        <div className='summary-card-meter'>
-          <p>Your result</p>
-          <div className='summary-card-meter-circle'>
-            <p>76</p>
-            <p>of 100</p>
-          </div>
-          <p>Great</p>
-          <p>
-            You scored higher than 65% of the people who have taken these tests.
-          </p>
-        </div>
-        <div>
-          <h3>Summary</h3>
-          <div>
-            <img alt='' src={reactionSvg} />
-            <p>Reaction</p>
+    <>
+      <div className='summary-card-wrapper'>
+        <div className='summary-card'>
+          <div className='summary-card-meter'>
+            <p>Your result</p>
+            <div className='summary-card-meter-circle'>
+              <p>76</p>
+              <p>of 100</p>
+            </div>
+            <p className='summary-card-meter-sentiment'>Great</p>
             <p>
-              <strong>80</strong> / 100
+              You scored higher than 65% of the people who have taken these
+              tests.
             </p>
           </div>
-          <div>
-            <img alt='' src={memorySvg} />
-            <p>Memory</p>
-            <p>
-              <strong>92</strong> / 100
-            </p>
-          </div>
-          <div>
-            <img alt='' src={verbalSvg} />
-            <p>Verbal</p>
-            <p>
-              <strong>61</strong> / 100
-            </p>
-          </div>
-          <div>
-            <img alt='' src={visualSvg} />
-            <p>Visual</p>
-            <p>
-              <strong>72</strong> / 100
-            </p>
+          <div className='summary-card-score-breakdown-wrapper'>
+            <h3>Summary</h3>
+            <div className='summary-card-score-breakdown-row reaction'>
+              <div className='summary-card-score-break-down-topic'>
+                <img alt='' src={reactionSvg} />
+                <p>Reaction</p>
+              </div>
+              <p className='summary-card-score-breakdown-row-score'>
+                <span>80</span> / 100
+              </p>
+            </div>
+            <div className='summary-card-score-breakdown-row memory'>
+              <div className='summary-card-score-break-down-topic'>
+                <img alt='' src={memorySvg} />
+                <p>Memory</p>
+              </div>
+              <p className='summary-card-score-breakdown-row-score'>
+                <span>92</span> / 100
+              </p>
+            </div>
+            <div className='summary-card-score-breakdown-row verbal'>
+              <div className='summary-card-score-break-down-topic'>
+                <img alt='' src={verbalSvg} />
+                <p>Verbal</p>
+              </div>
+              <p className='summary-card-score-breakdown-row-score'>
+                <span>61</span> / 100
+              </p>
+            </div>
+            <div className='summary-card-score-breakdown-row visual'>
+              <div className='summary-card-score-break-down-topic'>
+                <img alt='' src={visualSvg} />
+                <p>Visual</p>
+              </div>
+              <p className='summary-card-score-breakdown-row-score'>
+                <span>72</span> / 100
+              </p>
+            </div>
+            <button>Continue</button>
           </div>
         </div>
       </div>
-    </div>
+      <div className='too-small-screen'>
+        <p>Uh oh</p>
+        <p>This page is only able to be viewed on a screen 320px or wider</p>
+      </div>
+    </>
   )
 }
