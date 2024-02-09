40698309fbb463a032195394e96d639cabff728f
Diegopie <ishtaracademy@outlook.com>
2024-02-08 19:32:43 -0700

    working on card, updating fonts and text color

--- src/BlackPage/BlackPage.jsx
+++ src/BlackPage/BlackPage.jsx
@@ -7,7 +7,7 @@ export default function BlackPage () {
       <section className='BlackPage-Card-Container'>
       <article className='BlackPage-Card'>
         <img className="BlackPage-Card-Img" src="https://picsum.photos/200" alt="Jessica Randall is an experienced web developer from London England" />
-        <h2>Jessica Randall</h2>
+        <h2 className='BlackPage-Card-Title'>Jessica Randall</h2>
         <h3>London, United Kingdom</h3>
         <p>&quot;Front-end developer and avid reader.&quot;</p>
         <button>Github</button>
